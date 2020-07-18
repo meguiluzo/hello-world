62681dc033f658fdfd611309030bd9cd0ba34cbc
Manuel Eguiluz <eguiluz.manuel@gmail.com>
2020-07-18 15:34:33 -0500

    add helloworld-microprofile project

---
+++ helloworld-microprofile/src/main/java/com/example/microprofile/helloworld/MicroprofilehelloworldRestApplication.java
@@ -0,0 +1,11 @@
+package com.example.microprofile.helloworld;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MicroprofilehelloworldRestApplication extends Application {
+}
